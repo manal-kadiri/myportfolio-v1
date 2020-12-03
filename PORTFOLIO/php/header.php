5411942dc14ebb78bfb478e03195166fa164d27e
manal-kadiri <69117640+manal-kadiri@users.noreply.github.com>
2020-12-03 12:00:50 +0100

    Add files via upload

---
+++ PORTFOLIO/php/header.php
@@ -0,0 +1,29 @@
+
+    <header class="header_area">
+        <div class="main-menu">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#"><img src="./img/graphic1.jpg" width="100" height="80" alt="logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mr-auto"></div>  
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">About me</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">My projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                      </li>
+
+                  </ul>
+                </div>
+              </nav>
+        </div>
+    </header>
